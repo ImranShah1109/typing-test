2117881ed6db896300b79585aaa524f8d1156753
Imran <imranshaha41@gmail.com>
2023-05-24 13:50:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { GlobalStyles } from "./Styles/global";
+
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles/>
+      Hello
+    </div>
+  );
+}
+
+export default App;
