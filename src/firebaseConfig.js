f5a2bc04d583de7077fd174955fa3361ee800d30
Imran <imranshaha41@gmail.com>
2023-06-04 20:45:09 +0530

    configure firebase in project

---
+++ src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkIJ3mBWCnwLgjSxfNBwAaXhL3gicKoUY",
+    authDomain: "typing-speed-test-f4ee1.firebaseapp.com",
+    projectId: "typing-speed-test-f4ee1",
+    storageBucket: "typing-speed-test-f4ee1.appspot.com",
+    messagingSenderId: "539594593457",
+    appId: "1:539594593457:web:82364245739b931208fe90",
+    measurementId: "G-VZ1GQ8VYV5"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebaseApp.firestore();
+
+export {auth, db}
